9b0652472edc8742511c183f36add9da061e76d8
DmitriyLeybel <dmleybel@gmail.com>
2015-10-02 17:59:43 -0700

    Replaced build code and fillAverages with the more compact versions from rickExamples.ipynb. Made the overall Movies.py more pythonic. Included methods for checking the RMSE over varying dimensions and a given proportion of randomized test values

--- Movies.py
+++ Movies.py
@@ -1,41 +1,111 @@
 from sklearn.decomposition import TruncatedSVD
 import scipy as sp
+from random import *
 
-rdata = sp.loadtxt('u.data',int )
+class mPredictor():
 
-a = sp.zeros([100000,4])
-v = sp.array([])
-m = sp.array([],dtype=int)
-vxm = sp.empty([943,1682])
+    def __init__(self,file,dims,):
+        self.dims = dims
+        self.rdata = sp.loadtxt(file,int)
+        self.v = sp.array([])
+        self.m = sp.array([],dtype=int)
+        self.vxm = sp.empty(self.dims)
+
+    def buildMatrix(self):
+
+        cols = max(self.rdata[:,1]) # another way of doing it...
+        rows = max(self.rdata[:,0])
+        self.vxm = sp.zeros((rows,cols))
+        for rating in self.rdata:
+            self.vxm[rating[0]-1, rating[1]-1] = rating[2]
 
 
-for x in rdata[:,1]: # Builds the movie vector
-    if x not in m:
-        m = sp.append(m,x)
+    def fillAverages(self,vxm):
 
-for x in rdata[:,0]: # Builds the viewer vector
-    if x not in v:
-        v = sp.append(v,x)
+        vxmc = sp.copy(vxm)
+        for i in range(vxmc.shape[0]):
+            row = vxmc[i,:]
+            row[row==0] = sp.mean(row[row!=0])
+            vxmc[i,:] = row
+        return vxmc
 
-for x in rdata: # Creates the matrix containing the ratings of the movies by the viewers
-    r = x[2]
-    mov = sp.nonzero(m==x[1])
-    view = sp.nonzero(v==x[0])
-    vxm[view,mov] = r
+    def predict(self,cut, vxm=None):
 
-for x in vxm.T: # For unrated slots, it takes the movie's average rating and replaces the zeros with that
-    avg = sp.average(x[x>0])
-    for y,z in zip(x, range(len(x))):
-        if y == 0:
-            x[z]=avg
+        if vxm is None:
+            vxm = self.vxm
+            self.buildMatrix()
+            print(vxm)
+            vxm = self.fillAverages(vxm)
 
-U,s,V = sp.linalg.svd(vxm, full_matrices = True )
 
-U = U[:,0:20]
-V = V[0:20,:]
-S = sp.diag(s)
-S = S[:20,:20]
+        U, s, V = sp.linalg.svd(vxm, full_matrices = True )
 
-new_vxm = sp.dot(U, sp.dot(S,V))
+        U = U[:,0:cut]
+        V = V[0:cut,:]
+        S = sp.diag(s)
+        S = S[:cut,:cut]
+
+        new_vxm = sp.dot(U, sp.dot(S, V))
+        return new_vxm
+
+    def errorApproximation(self, ratio, dim=20):
+
+        self.buildMatrix()
+
+        sumNonzeros = (self.vxm !=0).sum()
+        numTest = int(ratio*sumNonzeros)
+
+        elementList = []
+
+        nonZeroTuple = sp.nonzero(mat.vxm)
+
+        for x in range(int(numTest)):
+            rInt = sp.random.randint(0,nonZeroTuple[0].size)
+            randrow = nonZeroTuple[0][rInt]
+            randcolumn = nonZeroTuple[1][rInt]
+
+            valElementIndex = [randrow,randcolumn]
+            elementList.append(valElementIndex)
+
+        self.modvxm = sp.copy(self.vxm)
+
+        for x in elementList:
+            self.modvxm[x[0],x[1]] = 0
+
+        self.fillAverages(vxm = self.modvxm)
+        self.newmodvxm = self.predict(dim,vxm=self.modvxm)
+
+        sqDiff = 0
+        for x in elementList:
+            sqDiff += sp.square(self.newmodvxm[x[0],x[1]] - self.vxm[x[0],x[1]])
+        self.rmse = sp.sqrt(sqDiff/len(elementList))
+
+        # print(self.vxm[elementList[0][0],elementList[1][0]])
+        # print(self.modvxm[elementList[0][0],elementList[1][0]])
+        # print(self.newmodvxm[elementList[0][0],elementList[1][0]])
+        # print(self.rmse)
+
+
+# mat = mPredictor('u.data',[943,1682])
+
+# x = mat.predict(cut=20)
+
+# mat.errorApproximation(.15,dim=5)
+# print(mat.rmse)
+dimList = []
+for x in [1,2,3,4,5,10,40,100,200,400,800]:
+    mat = mPredictor('u.data',[943,1682])
+    mat.errorApproximation(.15,dim=x)
+    print(x,': ',mat.rmse)
+    dimList.append(mat.rmse)
+
+
+
+
+
+
+
+
+
 
 
